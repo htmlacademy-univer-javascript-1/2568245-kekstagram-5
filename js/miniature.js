b5ecb22b1ad7eda6453addbb578d0e2a13da9df0
Alexander <alexmenshikov7@gmail.com>
2025-01-14 14:47:32 +0500

    added miniature template

---
+++ js/miniature.js
@@ -0,0 +1,26 @@
+const miniatureTemplate = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createMiniature = ({ comments, description, likes, url }) => {
+  const miniature = miniatureTemplate.cloneNode(true);
+
+  miniature.querySelector('.picture__img').src = url;
+  miniature.querySelector('.picture__img').alt = description;
+  miniature.querySelector('.picture__comments').textContent = comments.length;
+  miniature.querySelector('.picture__likes').textContent = likes;
+
+  return miniature;
+};
+
+const renderMiniatures = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const miniature = createMiniature(picture);
+    fragment.append(miniature);
+  });
+  container.append(fragment);
+};
+
+export { renderMiniatures };
